11cbf9de7bcf5b070140f8e27b175109dc3e8af8
ROEL2407 <roel24komen@gmail.com>
2021-10-26 12:54:00 +0200

    fetch data from json

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-import data from './dataset_raw.json';
-console.log(data);
+fetch("./dataset_raw.json")
+  .then(function (response) {
+    // The JSON data will arrive here
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    // If an error occured, it will be catched here
+    console.log(err);
+  });
+
+  function appendData(data) {
+    var mainContainer = document.getElementById("dataset");
+    for (var i = 0; i < data.length; i++) {
+        // Loops all items of json file to html 
+      console.log(data);
+    }
+  }
