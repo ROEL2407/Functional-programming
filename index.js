211ac385dc5eda7f749b591e6a53538a7728cc25
ROEL2407 <roel24komen@gmail.com>
2021-10-27 10:25:05 +0200

    Show keys in console + code cleanup

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 fetch("./dataset_raw.json")
-  .then(function (response) {
-    // The JSON data will arrive here
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    // If an error occured, it will be catched here
-    console.log(err);
-  });
+    .then(function (response) {
+        // The JSON data will arrive here
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+        let keys = getKeys(data);
+    })
+    .catch(function (err) {
+        // If an error occured, it will be catched here
+        console.log(err);
+    });
 
-  function appendData(data) {
+function getKeys(data) {
+    // Show all keys(questions) of the dataset
+    let keys = Object.keys(data[0])
+    console.log("Keys: ", keys);
+}
+
+function appendData(data) {
     var mainContainer = document.getElementById("dataset");
     for (var i = 0; i < data.length; i++) {
-        // Loops all items of json file to html 
-      console.log(data);
+        // Loops all items of json file to the console 
+        console.log(data);
     }
-  }
+}
