309ecde20c9f459df037c8622175e9ec2992d033
ROEL2407 <roel24komen@gmail.com>
2021-10-28 14:59:37 +0200

    version 1 of changing capitals to lower case

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mydata = fetch("./dataset_raw.json")
+fetch("./dataset_raw.json")
     .then(function (response) {
         // The JSON data will arrive here
         return response.json();
     })
     .then(function (data) {
         renameKeys(data);
-        dataToLowercase(data);
+        dataToLowerCase(data);
         showAllData(data);
         showKeys(data);
     })
@@ -21,17 +21,20 @@ function showAllData(data) {
     }
 }
 
-// function renameKey(obj, oldKey, newKey) {
-//     delete Object.assign(obj, { [newKey]: obj[oldKey] })[oldKey];
-// }
+function renameKey(obj, oldKey, newKey) {
+    delete Object.assign(obj, {
+        [newKey]: obj[oldKey]
+    })[oldKey];
+}
 
 function renameKeys(data) {
+    data.forEach(obj => renameKey(obj, 'Wat is je favoriete windrichting?', 'windrichting'));
     data.forEach(obj => renameKey(obj, 'Wat is je favoriete soort huisdier?', 'huisdier'));
-    data.forEach(obj => renameKey(obj, 'Wat is je favoriete windrichting?', 'oogkleur'));
+    data.forEach(obj => renameKey(obj, 'Wat is je oogkleur?', 'oogkleur'));
     data.forEach(obj => renameKey(obj, 'Op een schaal van 1 tot 10, hoeveel zin heb je in de Tech Track?', 'motivatie'));
     data.forEach(obj => renameKey(obj, 'Kies zelf of je deze vraag beantwoord.', 'upToYou'));
     data.forEach(obj => renameKey(obj, 'Wat is je favoriete datum?', 'date'));
-    data.forEach(obj => renameKey(obj, 'Wat is je favoriete datum?', 'dateText'));
+    data.forEach(obj => renameKey(obj, 'Wat is je favoriete datum, maar nu in tekst!', 'dateText'));
     data.forEach(obj => renameKey(obj, 'Wat is je favoriete zuivelproduct?', 'zuivel'));
     data.forEach(obj => renameKey(obj, 'Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)', 'kleding'));
     data.forEach(obj => renameKey(obj, 'Op welke verdieping van het TTH studeer je het liefst?', 'verdieping'));
@@ -39,14 +42,25 @@ function renameKeys(data) {
     data.forEach(obj => renameKey(obj, 'Wat wilde je later worden als je groot bent, maar nu toen je zelf 8 jaar was.', 'droombaan8'));
     data.forEach(obj => renameKey(obj, 'Kaas is ook een zoogdier?', 'kaas'));
     data.forEach(obj => renameKey(obj, 'Als je later een auto zou kopen, van welk merk zou deze dan zijn?', 'auto'));
-    console.log(data['Kaas is ook een zoogdier?']);
 }
 
+function dataToLowerCase(obj) {
+    for (var i = 0; i < obj.length; i++) {
+        Object.keys(obj[i]).forEach(key => {
+            console.log(obj[i][key]);
+            if (typeof obj[i][key] == 'string') {
+                var temp = obj[i][key].toLowerCase();
+                console.log(temp);
+            }
+        })
+    }
+}
 
 function showKeys(data) {
     // Show all keys(questions) of the dataset
-    let keys = Object.keys(data[0])
+    let keys = Object.keys(data[0]);
     console.log("Keys: ", keys);
+    return keys;
 }
 // let endpoint = './dataset_raw.json';
 
