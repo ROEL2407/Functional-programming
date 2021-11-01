951f504e859c5c23cce02b10fb456768a9f83e6d
ROEL2407 <roel24komen@gmail.com>
2021-11-01 13:52:08 +0100

    show names in table in console

--- api.js
+++ api.js
@@ -6,16 +6,36 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0")
     .then(function (data) {
         // Functions will be executed here
         showAllNames(data.results);
+        showStats(data.results)
+        dataArray(data.results);
+        console.table(data.results);
     })
     .catch(function (err) {
         // If an error occured, it will be catched here
         console.log(err);
     });
-
+ let pokemonArray = [];
 
 function showAllNames(data) {
     for (let i = 0; i < data.length; i++) {
         // shows all items of API request to the console
-        console.log(data[i].name);
+        var names = (data[i].name);
+        return names;
     }
+}
+
+function showStats(data) {
+    for (let i = 0; i < data.length; i++) {
+        var stats = data[i].stats;
+        console.log(stats);
+        
+    }
+
+}
+
+function dataArray (data) {
+    for (let i = 0; i < data.length; i++) {
+        pokemonArray.push(data[i].name);
+    }
+    console.table(pokemonArray);
 }
