07239a1322aeb0fde6da3756390f2509666b45c1
ROEL2407 <roel24komen@gmail.com>
2021-11-02 12:57:32 +0100

    refactor: fetch, fix: chained fetch

--- api.js
+++ api.js
@@ -1,31 +1,46 @@
-fetch("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0")
-    .then(function (response) {
-        // The JSON data will arrive here
-        return response.json();
-    })
-    .then(function (data) {
-        // Functions will be executed here
-        let results = data.results;
-        let allPokemonArray = [];
-        showAllNames(results);
-        showStats(results)
-        dataArray(results);
-        console.table(results);
+function starters() {
+    //fetch init API
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0")
+        .then(response => response.json())
+        .then(data => getPokemonData(data))
+        .then(function (data) {
+            let results = data.results;
+            showAllNames(results);
+            dataArray(results);
+        })
+}
+    
+function getPokemonData(data) {
+    let urls = [];
+    let promises = [];
+    let pokemons = [];
 
-        for (let i = 0; i < results.length; i++) {
-            let temp = results[i].url;
-            allPokemonArray.push(temp);
-        }
-        console.table('urls:', allPokemonArray);
-        let p = Promise.all(allPokemonArray);
-        console.log(p);
-
-        
-    })
-    .catch(function (err) {
-        // If an error occured, it will be catched here
-        console.log(err);
+    data.results.forEach(result => {
+        //push all urls into array urls
+        urls.push(result.url);
     });
+    
+    //for all urls, push a promise from the url to an array promises
+    for (let i = 0; i < urls.length; i++) {
+        promises.push(getPokemon(urls[i]));
+    }
+    //resolve all promises
+    Promise.all(promises).then(results => {
+        //for each pokemon in results push the pokemon to the array pokemons
+        results.forEach(pokemon => {
+            pokemons.push(pokemon);
+        });
+        console.log(pokemons);
+    });
+}
+    
+function getPokemon(url) {
+    // return every fetch
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data);
+}
+starters();
 
  let pokemon = [];
 
@@ -37,14 +52,12 @@ function showAllNames(data) {
     }
 }
 
-function showStats(data) {
-    for (let i = 0; i < data.length; i++) {
-        var stats = data[i].stats;
-        console.log(stats);
-        
-    }
-
-}
+// function showStats(data) {
+//     for (let i = 0; i < data.length; i++) {
+//         var stats = data[i].stats;
+//         console.log(stats);
+//     }
+// }
 
 function dataArray (data) {
     for (let i = 0; i < data.length; i++) {
